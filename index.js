45e64209c1161322e5ea740bfae334b32ed3c97a
Christian Perez <perezchristian1012@gmail.com>
2020-02-09 02:22:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const writeCards()
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
 
-const countDown()
+function countDown(number) {
+  while(number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
